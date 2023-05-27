0a866e723c1c06c2e08b09f0e141e5e5f4a5de77
GamingSpud <decortesa@outlook.com>
2023-05-26 19:43:28 -0500

    Initial implementation of system call list and FCFS print management

--- main.py
+++ main.py
@@ -10,9 +10,30 @@ sysCallB = SysCall(1)
 sysCallC = SysCall(5)
 sysCallD = SysCall(2)
 
+callList = []
+
+def callQueue(s):
+    callList.append(s)
+
+def callClear():
+    callList = []
+
 def FCFS(l):
-    for i in l:
-        for j in i.execTime:
-            print
+    lines = []
+    for i in range (0,len(l)*2,2):
+        lines.append("  |")
+        lines.append("---")
+        #for j in range (0,l[i].execTime):
+            #line1 = line1 + "T" + str(i+j) + "|"
+            #if i == 0 and j == 0:
+                
+            #line1 = line1 + "P" + str(i) + "|"
+        print(lines[i])
+        print(lines[i+1])
         
-    
+callQueue(sysCallA)
+callQueue(sysCallB)
+callQueue(sysCallC)
+callQueue(sysCallD)
+
+FCFS(callList)
